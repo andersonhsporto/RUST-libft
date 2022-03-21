6090395d0a4b887b549033518f33447bc50dbfbc
Anderson Porto <andersonhigo2@gmail.com>
2022-03-20 23:32:00 -0300

    build: start memset

---
+++ ru_memset.rs
@@ -0,0 +1,11 @@
+// fn ru_memset(slice: &mut [u8], byte: u8) {
+//     unsafe {
+//         ptr::write_bytes(slice.as_mut_ptr(), byte, slice.len());
+//     }
+// }
+
+// fn main() {
+// 	let teste2 = "teste";
+// 	ru_memset(teste2, 'a' as u8);
+// 	println!("{}", teste2);
+// }
